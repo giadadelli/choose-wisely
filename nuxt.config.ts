66766fe8817d046901e2deb815e843185545e787
Giada Delli <g.delli@techgap.it>
2026-07-12 12:57:25 +0200

    Configura lang=it centralmente in nuxt.config.ts
    Imposta app.head.htmlAttrs.lang = 'it' cosi' che <html lang="it">
    sia presente su tutte le pagine senza introdurre i18n multi-lingua,
    non necessario in MVP.
    
    SCRUM-10

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,13 @@
 export default defineNuxtConfig({
   modules: ['@nuxt/eslint'],
   devtools: { enabled: true },
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'it',
+      },
+    },
+  },
   css: ['~/assets/css/tokens.css'],
   compatibilityDate: '2025-07-15',
   eslint: {
